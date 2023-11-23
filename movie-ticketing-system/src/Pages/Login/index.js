81bdda3bfd5d694386d4d339328bbe7603283da9
Noreenshoke <esteradoricanoreen.aws@gmail.com>
2023-11-23 16:15:45 +0800

    3rd commit

--- movie-ticketing-system/src/Pages/Login/index.js
+++ movie-ticketing-system/src/Pages/Login/index.js
@@ -1,17 +1,25 @@
-import React from "react";
+import React, {useState}from "react";
 import "../../Stylesheets/Login.css";
 
 // import password_icon from "../../Assets/password.png";
 // import username_icon from "../../Assets/username.png";
 
 const Login = () => {
+
+    const [action,setAction] = useState("Register");
+
   return (
     <div className="container">
       <div className="header">
-        <div className="text">Login</div>
+        <div className="text">{action}</div>
         <div className="underline"></div>
       </div>
       <div className="inputs">
+        {action==="Login"?<div></div>:<div className="input">
+          <img src=" " alt="" />
+          <input placeholder="Name" type="name" />
+        </div>}
+      
         <div className="input">
           <img src=" " alt="" />
           <input placeholder="Username" type="username" />
@@ -21,10 +29,11 @@ const Login = () => {
           <input placeholder="Password" type="password" />
         </div>
       </div>
-      <div className="submit.container">
-        <div className="submit">
-           <button>Login</button>
-            </div>
+      
+      
+      <div className="submit-container">
+        <div className={action==="Login"?"submit gray":"submit"} onClick={()=>{setAction("Register")}}>Register</div>
+        <div className={action==="Register"?"submit gray":"submit"} onClick={() =>{setAction("Login")}}>Login</div>
       </div>
     </div>
   );
