af1a84e93cdce2df61758b2909e8003ee7fb5998
MathewDelCast <mathewdelcast@gmail.com>
2023-11-28 13:53:30 +0800

    Added Cancel Reservation 2x

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Login from "./Pages/Login"
 import Register from "./Pages/Register";
 import Menu from "./Pages/Menu";
 import Datelist from './Pages/Datelist';
+import CancelReservation from './Pages/CancelReservation';
 function App() {
   return (
     
@@ -13,37 +14,11 @@ function App() {
         <Routes>
             <Route path="menu" element={<Menu/>}></Route>
 
+            <Route path="cancelreservation" element={<CancelReservation/>}></Route>
 
             <Route path="login" element={<Login/>}></Route>
             <Route path="register" element={<Register/>}></Route>
-            
-            
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
             <Route path="datelist" element={<Datelist/>}></Route>
-
-
-
-
         </Routes>
       </BrowserRouter>
     </div>
