df48f716c3f4e0df06d54d81b690569194f24ec1
MathewDelCast <mathewdelcast@gmail.com>
2023-11-28 14:22:54 +0800

    Added improved dynamic search function

--- src/Pages/CancelReservation/index.js
+++ src/Pages/CancelReservation/index.js
@@ -1,40 +1,56 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import SearchBar from './SearchBar';
 import TicketList from './TicketList';
 import TicketModal from './TicketModal';
 import 'antd/dist/reset.css';
 import '../../Stylesheets/CancelReservation.css';
 
+// Initial ticket data outside the component
+const initialTicketData = [
+  {
+    ticketNumber: 'T12354',
+    description: 'Issue with login functionality',
+    status: 'Open',
+  },
+  {
+    ticketNumber: 'T12456',
+    description: 'Error in payment processing',
+    status: 'In Progress',
+  },
+  {
+    ticketNumber: 'T12367',
+    description: 'UI design improvements',
+    status: 'Closed',
+  },
+  // Add more ticket objects as needed
+];
+
 const CancelReservation = () => {
   const [searchText, setSearchText] = useState('');
-  const [ticketData, setTicketData] = useState();
+  const [ticketData, setTicketData] = useState(initialTicketData);
   const [selectedTicket, setSelectedTicket] = useState(null);
   const [isModalVisible, setIsModalVisible] = useState(false);
 
   const handleSearch = (text) => {
     setSearchText(text);
-    // Implement your search logic here and update ticketData accordingly
   };
 
-  const initialTicketData = [
-    {
-      ticketNumber: 'T12345',
-      description: 'Issue with login functionality',
-      status: 'Open',
-    },
-    {
-      ticketNumber: 'T12346',
-      description: 'Error in payment processing',
-      status: 'In Progress',
-    },
-    {
-      ticketNumber: 'T12347',
-      description: 'UI design improvements',
-      status: 'Closed',
-    },
-    // Add more ticket objects as needed
-  ];
+  useEffect(() => {
+    // If search text is empty, show all tickets
+    if (!searchText.trim()) {
+      setTicketData(initialTicketData);
+      return;
+    }
 
+    // Filter the initialTicketData based on the searchText
+    const filteredTickets = initialTicketData.filter(
+      (ticket) =>
+        ticket.ticketNumber.toLowerCase().includes(searchText.toLowerCase())
+    );
+
+    // Update the ticketData state with the filtered tickets
+    setTicketData(filteredTickets);
+  }, [searchText]);
 
   const handleCancelTicket = (ticket) => {
     setSelectedTicket(ticket);
@@ -48,7 +64,22 @@ const CancelReservation = () => {
 
   const handleModalConfirm = () => {
     // Implement logic to cancel the selected ticket
-    setIsModalVisible(false);
+    // Assuming cancelTicketApiCall is your actual API call function
+    cancelTicketApiCall(selectedTicket)
+      .then(() => {
+        const updatedTickets = ticketData.filter(
+          (ticket) => ticket.ticketNumber !== selectedTicket.ticketNumber
+        );
+
+        setTicketData(updatedTickets);
+        setSelectedTicket(null);
+        setIsModalVisible(false);
+      })
+      .catch((error) => {
+        console.error('Error canceling ticket:', error);
+        setSelectedTicket(null);
+        setIsModalVisible(false);
+      });
   };
 
   return (
