11ec29eebe9d8fa541c1654057a8fe3e35ad83b5
Joerge Ryan Lou <joergeryanlou.aws@gmail.com>
2023-11-30 15:34:38 +0800

    trial axios

--- src/Server/Server.js
+++ src/Server/Server.js
@@ -1,11 +1,29 @@
-
+const mongoose = require('mongoose');
 const express = require('express');
-const app = express();
-require("dotenv").config();
-const dbconfig = require('./dbConfig');
-const port = process.env.PORT || 5000;
 
+const userRouter = require('./UserRoute');
+const userController = require('./UserController');
+const app = express();
+const connection = mongoose.connection;
+
+mongoose.connect("mongodb+srv://Hanc81818:WM8XGRMcYMzaitRA@cluster0.azy2r3f.mongodb.net/MovieReservationTicket");
+
+connection.on('open', () => {
+  console.log("Connected successfully");
+});
+
+connection.on('error', (error) => {
+  console.log("Error in connection:", error);
+});
+
+
+app.use(express.json());
+app.use("/",userRouter); 
+
+const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Nodejs server is running port 5000`));
+
 
 //mongodb+srv://Hanc81818:WM8XGRMcYMzaitRA@cluster0.azy2r3f.mongodb.net/
 
+module.exports = {app,mongoose};
