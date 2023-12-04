19333fb691659906d4dd79a937fd35c093c66e28
MathewDelCast <mathewdelcast@gmail.com>
2023-12-04 14:27:36 +0800

    v9

--- client/src/Pages/apiCall/registerUserApi.js
+++ client/src/Pages/apiCall/registerUserApi.js
@@ -11,9 +11,6 @@ import { axiosInstance } from ".";
           }
     }
 
-
-
-
 // //Login a user
 
 // export const LoginUser = async(payload) =>{
@@ -34,40 +31,5 @@ import { axiosInstance } from ".";
 //       return error;
 //   }
 // }
-
-export default RegisterUser;import { axiosInstance } from ".";
-
-
-// //Register new user
-  export  const register = async() => {
-        try {
-            const response = await axiosInstance.post("api/users/register",payload);
-            return response.data;
-          } catch(error) {
-             return error.response;
-          }
-    }
-
-
-//Login a user
-
-export const LoginUser = async(payload) =>{
-  try{
-   const response = await axiosInstance.post("/api/users/login", payload);
-   return response.data;
-  } catch(error) {
-     return error.response;
-  }
-}
-
-// Get current user
-export const GetCurrentUser = async () => {
-  try {
-      const response = await axiosInstance.get("/api/users/get-current-user");
-      return response.data;
-  } catch (error) {
-      return error;
-  }
-}
 
 export default RegisterUser;
